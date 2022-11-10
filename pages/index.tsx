63402bc70e1ae5134369e74bdf7a456bbad1bb2a
ellisvelandia <eyis619@gmail.com>
2022-11-09 22:39:18 -0500

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title className="text-white bg-gray-900">Ellis Velandia Portfolio</title>
+      </Head>
+      {/* Header */}
+      {/* Hero */}
+      {/* About */}
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
+    </div>
+  );
+}
