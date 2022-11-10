3a0f7e7ef6b1714965e3a8fa7ee639631c49dfb6
ellisvelandia <eyis619@gmail.com>
2022-11-10 13:14:25 -0500

    profile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,10 +19,26 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text} </span>
-        <Cursor cursorColor="#f70a0a" />
-      </h1>
+      <img
+        className="relative rounded-full h-44 w-44 m-auto object-center		object-cover"
+        src="https://previews.dropbox.com/p/thumb/ABslME36SOhCU3522V0N3gRPKP4es8KCcnsgijpDqaKzLF04Ox-lnBsJggkV6bJTYHcfS5w8UvSSozz5x7pIiaOw2GKKVOBJMDlIg4sTAgT1HL2oUV3HMS4LpEznfNTD6bybBIDz7_XQFCJYUr5HEy6Lezr7VoZymcEjNkUW61XvKADx5Cja-UYR-AU6mp9XXk-b6wZO_V_97zXU7EMoSKO4myMNecHhh0KIw5fXWSw07I-0BsnkXUH0B99bBGVBIPcAF9MvpR_RCcWDNi5drfvdJSTVhYlkWzMcft0gHTXkMDx7k4P9_yiaLmfeh52IZU9ig7nXPQUwL7JznyuOmkytFk1Kf0ZSTBn1TISCoWtXDHBABV0Z_HSTiJQpfNBkOMo/p.png"
+        alt="image"
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text} </span>
+          <Cursor cursorColor="#f70a0a" />
+        </h1>
+        <div>
+          <button className="heroButton">About</button>
+          <button className="heroButton">Experience</button>
+          <button className="heroButton">Skills</button>
+          <button className="heroButton">Projects</button>
+        </div>
+      </div>
     </div>
   );
 }
